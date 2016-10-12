62679641cb73892697cd8379e4dcec7d17189ef2
Aleksey Khoroshev <aleksxor@gmail.com>
2016-10-12 13:23:36 +0700

    rebuild after rebase

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.581',
+  version: '0.0.582',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
