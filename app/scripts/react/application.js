42a4c7055055b06a3d32aed5dec6dab51df569d4
Aleksey Khoroshev <aleksxor@gmail.com>
2016-10-27 19:52:35 +0700

    quickfix custom html out of bountaries

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.629',
+  version: '0.0.630',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
