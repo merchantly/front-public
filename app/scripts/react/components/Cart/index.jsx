184b5ae7141c54ca3f217d5fdcbbf9b30eaac755
Aleksey Khoroshev <aleksxor@gmail.com>
2016-11-17 16:25:20 +0700

    cart, checkout widget components

--- app/scripts/react/components/Cart/index.jsx
+++ app/scripts/react/components/Cart/index.jsx
@@ -43,21 +43,15 @@ class CartContainer extends Component {
       initialCart,
       initPackages,
       initialPackages,
-      fetchCart,
-      fetchPackages,
     } = this.props;
 
     if (!storeInitialized && canUseDOM()) {
       if (initialCart) {
         initCart(initialCart);
-      } else {
-        fetchCart();
       }
 
       if (initialPackages) {
         initPackages(initialPackages);
-      } else {
-        fetchPackages();
       }
       storeInitialized = true;
     }
