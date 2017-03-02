48a7d5a939d1b47bde8b96cf29ee0c4b09f95807
Ananev Alexander <minoru.null@gmail.com>
2017-03-02 13:14:27 +0000

    rebuild after merge

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.697',
+  version: '0.0.698',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
