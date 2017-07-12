23426ca0439950a7d1efd6ddc5f5e5f763ecf25e
Danil Pismenny <danil@brandymint.ru>
2017-07-12 15:48:28 +0300

    dist

--- dist/scripts/public.prerender.development.js
+++ dist/scripts/public.prerender.development.js
@@ -126,9 +126,9 @@ ContentPagePage = require('./react/components/ContentPage/ContentPagePage');
 
 global.gon = {
   env: 'development',
-  operator_api_url: 'http://wannabe.vagrant.dev:3000/operator/api',
-  public_api_url: 'http://wannabe.vagrant.dev:3000/api',
-  thumbor_url: 'http://thumball.brandydev.ru',
+  //operator_api_url: '//wannabe.vagrant.dev:3000/operator/api',
+  //public_api_url: '//wannabe.vagrant.dev:3000/api',
+  thumbor_url: '//thumbor0.kiiiosk.ru',
   kiiiosk: true,
   max_items_count: 100,
   asset_host: 'assets.kiiiosk.ru'
