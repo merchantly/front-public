c1db1c60c0944391f3f88cde28d36b0a8b7f827c
Danil Pismenny <danil@brandymint.ru>
2017-07-12 15:48:18 +0300

    Отключил в настройках prerender gon.*_api и установил thumbor0

--- app/scripts/prerender.production.js
+++ app/scripts/prerender.production.js
@@ -1,8 +1,8 @@
 global.gon = {
   env: 'production',
-  operator_api_url: 'http://wannabe.kiiiosk.ru/operator/api',
-  public_api_url: 'http://wannabe.kiiiosk.ru/api',
-  thumbor_url: 'http://thumbor.kiiiosk.ru',
+  // operator_api_url: 'http://wannabe.kiiiosk.ru/operator/api',
+  // public_api_url: 'http://wannabe.kiiiosk.ru/api',
+  thumbor_url: '//thumbor2.kiiiosk.ru',
   kiiiosk: true,
   max_items_count: 100,
   asset_host: 'assets.kiiiosk.ru',
