41de26e0b30db3b433b240c02e5730020bb05c61
Danil Pismenny <azavgorodnev93@mail.ru>
2018-09-24 20:28:37 +0000

    вернул b-slider

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.898',
+  version: '0.0.906',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
