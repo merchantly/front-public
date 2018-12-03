cf87947895fa6320031856bbc50a8ca95568017b
Mikhail <mm.leontev@gmail.com>
2018-12-03 13:41:40 +0300

    wip

--- app/scripts/react/components/DesignSettings/DesignSettingsSlider.jsx
+++ app/scripts/react/components/DesignSettings/DesignSettingsSlider.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Slider from '../common/Slider';
+// import Slider from '../common/Slider';
+import { Range } from 'rc-slider';
+
+const MINIMUM_VALUE = 0,
+      MAXIMUM_VALUE = 100,
+      STEP = 1;
 
 export default class DesignSettingsSlider extends Component {
   static propTypes = {
@@ -10,8 +15,15 @@ export default class DesignSettingsSlider extends Component {
     onChange: PropTypes.func.isRequired,
     step: PropTypes.number,
     to: PropTypes.number,
-    value: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired, 
   }
+
+  static defaultProps = {
+    from: MINIMUM_VALUE,
+    to: MAXIMUM_VALUE,
+    step: STEP
+  }
+
   handleSlide(range) {
     this.props.onChange(parseFloat(range[0]));
   }
@@ -23,10 +35,12 @@ export default class DesignSettingsSlider extends Component {
 
     return (
       <span className={sliderClasses}>
-        <Slider
-          {...this.props}
+        <Range
+          min={this.props.from}
+          max={this.props.to}
+          defaultValue={[this.props.from, this.props.to]}
           onChange={this.handleSlide.bind(this)}
-          onSlide={this.handleSlide.bind(this)}
+          // onSlide={this.handleSlide.bind(this)}
         />
         {displayValue &&
           <span className="design-settings__slider-value">
