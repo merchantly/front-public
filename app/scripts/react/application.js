035b15bfc9987c4b94985550053866bbd6e8f596
Danil Pismenny <azavgorodnev93@mail.ru>
2018-12-12 11:48:50 +0000

    fix translate cabinet

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.953',
+  version: '0.0.954',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
--- app/scripts/react/components/Cabinet/CabinetPage.jsx
+++ app/scripts/react/components/Cabinet/CabinetPage.jsx
@@ -26,6 +26,7 @@ class CabinetPage extends Component {
           orders,
           clientForm,
           companyForm,
+          i18n,
         }} />
       </VendorLayoutContainer>
     );
