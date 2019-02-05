5f4e738600a660b8c1c11b2e9a45276d96b50b10
Danil Pismenny <azavgorodnev93@mail.ru>
2019-02-05 11:17:10 +0000

    rebase

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.961',
+  version: '0.0.962',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
