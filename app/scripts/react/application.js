fc025c59fe2b023b2aa57ba5f909df6849bdfc2d
Alex Zavgorodnev <azavgorodnev93@mail.ru>
2019-03-14 07:05:09 +0000

    [#2834]  Починить надпись сделано на коиске

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.970',
+  version: '0.0.971',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
