0d9c6bf681c483933658eb634823de8897716ba4
Danil Pismenny <danil@brandymint.ru>
2019-03-27 18:04:45 +0300

    fix tests

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.1.20',
+  version: '0.1.21',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
