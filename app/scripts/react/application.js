1a4940136c2bf6c4fb015c402be053df04b75628
Danil Pismenny <danil@brandymint.ru>
2019-03-27 15:03:23 +0300

    m

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.1.11',
+  version: '0.1.12',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
