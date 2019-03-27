cee642b6d3179021610d95d37a11565cc0c4b7a0
dapi <danil@brandymint.ru>
2019-03-27 18:28:17 +0300

    hotfix order page

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.1.22',
+  version: '0.1.23',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
--- app/scripts/react/components/Checkout/index.jsx
+++ app/scripts/react/components/Checkout/index.jsx
@@ -31,6 +31,7 @@ class Checkout extends Component {
       publicOffer,
       submitOrderUrl,
       t,
+      cart,
     } = this.props;
 
     return (
