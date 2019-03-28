e362adfed746bb69b7073c6f59a0da6e0597e7cb
Alex Zavgorodnev <azavgorodnev93@mail.ru>
2019-03-28 10:57:51 +0000

    dist

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -24,7 +24,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.1.26',
+  version: '0.1.27',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
