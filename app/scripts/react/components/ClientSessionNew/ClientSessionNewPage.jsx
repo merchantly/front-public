b190e09b29e61647a572292a1308d2f2a06b2f43
Alex Zavgorodnev <azavgorodnev93@mail.ru>
2019-04-10 13:48:13 +0000

    minor fix

--- app/scripts/react/components/ClientSessionNew/ClientSessionNewPage.jsx
+++ app/scripts/react/components/ClientSessionNew/ClientSessionNewPage.jsx
@@ -14,6 +14,7 @@ class ClientSessionNewPage extends Component {
       vendorClientSessionsPath,
       vendorClientRegistrationPath,
       phoneValue,
+      clientRegistrationButtonText,
     } = this.props;
 
     return (
