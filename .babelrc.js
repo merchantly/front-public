e54f3cfb93bcb79ebe43ddf8bcd0fe369792dc32
Danil Pismenny <danil@brandymint.ru>
2019-05-25 17:18:41 +0300

    tests

--- .babelrc.js
+++ .babelrc.js
@@ -19,14 +19,6 @@ module.exports = {
         "rc": "./app/scripts/react/components",
         "test": "./test",
         "styles": "./app/stylesheets",
-        //'jQuery.mmenu': './node_modules/@bower_components/jQuery.mmenu/src/js/jquery.mmenu.min.all',
-        //'bootstrap': 'node_modules/@bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
-        //'fancybox': './node_modules/@bower_components/fancybox/source/jquery.fancybox',
-        //'fancybox.wannabe': './node_modules/@bower_components/fancybox-wannabe-fix/index',
-        //'jquery': './node_modules/@bower_components/jquery/dist/jquery',
-        //'owlCarousel': './node_modules/@bower_components/OwlCarousel/owl-carousel/owl.carousel',
-        //'jquery.role': './node_modules/@bower_components/jquery.role/lib/jquery.role',
-        //'sticky-kit': './node_modules/@bower_components/sticky-kit/jquery.sticky-kit'
       }
     }]
   ]
