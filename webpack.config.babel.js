e9706914129584cad02543549cbb9931031a2f03
Danil Pismenny <danil@brandymint.ru>
2019-05-31 01:13:47 +0300

    fix svg icons

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -65,7 +65,7 @@ var rules = [
     ]
   },
   {
-    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+    test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
     use: [{
       loader: 'file-loader',
       options: {
