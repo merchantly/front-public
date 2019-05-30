42b75dcbf2bc02b466043c6b13f55ade8623f283
Danil Pismenny <danil@brandymint.ru>
2019-05-30 09:29:08 +0300

    ruby tests

---
+++ test/mini_racer_test.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require 'mini_racer'
+
+FILE = './dist/store_app_node.js'
+
+context = MiniRacer::Context.new
+context.eval "var global = global || this;"
+context.eval "var self = self || this;"
+context.eval File.read FILE
