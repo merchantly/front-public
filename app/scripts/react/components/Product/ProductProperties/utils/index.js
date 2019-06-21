94d4e26831e8817fc803ca33f90ebe80fcdce76b
Danil Pismenny <danil@brandymint.ru>
2019-06-21 11:10:49 +0300

    update chai to 4.2

--- app/scripts/react/components/Product/ProductProperties/utils/index.js
+++ app/scripts/react/components/Product/ProductProperties/utils/index.js
@@ -28,7 +28,7 @@
 //   ]
 // }
 
-import { includes } from 'lodash-es';
+import { includes } from 'lodash-es/includes';
 
 export function getOptions(properties, goods, filters) {
   return properties.reduce((prev, property) => {
